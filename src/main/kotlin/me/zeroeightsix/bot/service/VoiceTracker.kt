788498a16bdadfa6b6350fe8cb6253bf13324a8a
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2021-04-09 16:45:47 +0200

    Use `Instant` and `Duration` in VoiceTracker
    Also removed small debug line, renamed to VoiceTracker from VoiceChatTracker.

--- src/main/kotlin/me/zeroeightsix/bot/service/VoiceTracker.kt
+++ src/main/kotlin/me/zeroeightsix/bot/service/VoiceTracker.kt
@@ -15,10 +15,12 @@ import org.ktorm.entity.filter
 import org.ktorm.entity.find
 import org.ktorm.entity.toList
 import org.ktorm.support.mysql.insertOrUpdate
+import java.time.Duration
+import java.time.Instant
 
-object VoiceChatTracker {
+object VoiceTracker {
 
-    private val userTimeMap = mutableMapOf<MemberID, Long>()
+    private val userTimeMap = mutableMapOf<MemberID, Instant>()
 
     init {
         jda.listener<GuildVoiceUpdateEvent> { event ->
@@ -37,11 +39,12 @@ object VoiceChatTracker {
                     flushTime(memberId, channelLeft.idLong)
                 }
 
-                userTimeMap[memberId] = System.currentTimeMillis()
+                userTimeMap[memberId] = Instant.now()
             }
         }
     }
 
+    // TODO: Cache these? We can easily invalidate cache (or even just update) on every flush.
     fun databaseGetTime(member: MemberID) = database.voiceChatTimes.find { it.memberId eq member }
 
     fun databaseGetTime(member: MemberID, channelId: Long) =
@@ -50,18 +53,17 @@ object VoiceChatTracker {
         }.toList()
 
     private fun flushTime(memberId: Long, channelId: Long) {
-        val temp = userTimeMap.remove(memberId)
-        println("flush: $memberId - $temp")
-        val timeJoined = temp ?: return
-        val timeSpent = System.currentTimeMillis() - timeJoined
+        val timeJoined = userTimeMap.remove(memberId) ?: return
+        val timeSpent = Duration.between(timeJoined, Instant.now())
+        val timeSpentMillis = timeSpent.toMillis()
 
         transaction {
             insertOrUpdate(VoiceChatTimes) {
                 set(it.memberId, memberId)
                 set(it.channelId, channelId)
-                set(it.timeSpent, timeSpent)
+                set(it.timeSpent, timeSpentMillis)
                 onDuplicateKey {
-                    set(it.timeSpent, it.timeSpent + timeSpent)
+                    set(it.timeSpent, it.timeSpent + timeSpentMillis)
                 }
             }
         }
