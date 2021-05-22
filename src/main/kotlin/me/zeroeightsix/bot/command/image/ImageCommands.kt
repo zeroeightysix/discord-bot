447d83bb29697c1694d3609e5c1220f5846cb7f7
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2021-05-22 15:40:54 +0200

    Refactor ImageCommands and BlurCommand into generic FilterCommand

--- src/main/kotlin/me/zeroeightsix/bot/command/image/ImageCommands.kt
+++ src/main/kotlin/me/zeroeightsix/bot/command/image/ImageCommands.kt
@@ -3,26 +3,29 @@
 package me.zeroeightsix.bot.command.image
 
 import com.sksamuel.scrimage.ImmutableImage
-import com.sksamuel.scrimage.filter.Filter
 import com.sksamuel.scrimage.nio.GifSequenceWriter
 import com.sksamuel.scrimage.nio.JpegWriter
 import dev.minn.jda.ktx.await
 import me.zeroeightsix.bot.command.CommandContext
 import me.zeroeightsix.bot.image.BoxBlurFilter
 import me.zeroeightsix.bot.image.GifReader
-import me.zeroeightsix.bot.util.BUNDLE
 import me.zeroeightsix.bot.util.Conversation.nextFile
 import me.zeroeightsix.bot.util.tryDelete
 import me.zeroeightsix.bot.util.withoutFileExtension
-import org.jetbrains.annotations.PropertyKey
 import java.io.ByteArrayInputStream
 
-abstract class ImageCommand(@PropertyKey(resourceBundle = BUNDLE) private val supplyTranslation: String) {
+object FilterCommand {
 
-    abstract val filter: Filter
+    suspend fun CommandContext.execute(usrFilter: String) {
+        val filter = when (usrFilter) {
+            "blur" -> BoxBlurFilter(10f, 10f, 8)
+            else -> {
+                event.reply(translate("unknown_filter").progressBorked).await()
+                return
+            }
+        }
 
-    suspend fun CommandContext.execute() {
-        val reply = event.reply(translate(supplyTranslation).progressInput).await()
+        val reply = event.reply(translate("supply_image").progressInput).await()
 
         @Suppress("BlockingMethodInNonBlockingContext")
         nextFile({ it.isImage }) { event, attachment ->
@@ -58,9 +61,4 @@ abstract class ImageCommand(@PropertyKey(resourceBundle = BUNDLE) private val su
         }
     }
 
-}
-
-object BlurCommand : ImageCommand("supply_image") {
-    override val filter: Filter
-        get() = BoxBlurFilter(10.0f, 10.0f, 8)
 }
